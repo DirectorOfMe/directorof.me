6fa47130bd1ab255b4565b295a78d7931ab24f82
Matthew Story <matt@directorof.me>
2018-03-14 05:25:25 +0000

    [DOM-9] actually verify email
    Signed-off-by: Matthew Story <matt@directorof.me>

--- apis/auth/directorofme_auth/resources/authenticate.py
+++ apis/auth/directorofme_auth/resources/authenticate.py
@@ -1,81 +1,93 @@
-'''
->>> # Credentials you get from registering a new application
->>> client_id = '<the id you get from google>.apps.googleusercontent.com'
->>> client_secret = '<the secret you get from google>'
->>> redirect_uri = 'https://your.registered/callback'
-
->>> # OAuth endpoints given in the Google API documentation
->>> authorization_base_url = "https://accounts.google.com/o/oauth2/v2/auth"
->>> token_url = "https://www.googleapis.com/oauth2/v4/token"
->>> scope = [
-...     "https://www.googleapis.com/auth/userinfo.email",
-...     "https://www.googleapis.com/auth/userinfo.profile"
-... ]
-
->>> from requests_oauthlib import OAuth2Session
->>> google = OAuth2Session(client_id, scope=scope, redirect_uri=redirect_uri)
-
->>> # Redirect user to Google for authorization
->>> authorization_url, state = google.authorization_url(authorization_base_url,
-...     # offline for refresh token
-...     # force to always make user click authorize
-...     access_type="offline", prompt="select_account")
->>> print 'Please go here and authorize,', authorization_url
-
->>> # Get the authorization verifier code from the callback url
->>> redirect_response = raw_input('Paste the full redirect URL here:')
-
->>> # Fetch the access token
->>> google.fetch_token(token_url, client_secret=client_secret,
-...         authorization_response=redirect_response)
-
->>> # Fetch a protected resource, i.e. user profile
->>> r = google.get('https://www.googleapis.com/oauth2/v1/userinfo')
->>> print r.content
-'''
 import flask
+import functools
 
 from requests_oauthlib import OAuth2Session
-from flask_restful import Resource, fields, marshal_with, abort
+from flask_restful import Resource, fields, marshal_with, abort, reqparse
+from oauthlib.oauth2 import OAuth2Error
 
 from directorofme_flask_restful import resource_url
 
 from . import api, config
 
 ### TODO: offline for integrations, but not for auth
-@resource_url(api, "/authenticate/oauth/<string:service>", endpoint="oauth_api")
-class OAuth(Resource):
-    def get(self, service):
+def with_service_client(fn):
+    @functools.wraps(fn)
+    def inner(obj, service, *args, **kwargs):
         client = None
-
         if service == "google":
             client = Google()
         else:
             return abort(404, message="no oauth service named {}".format(service))
 
-        url = client.auth_redirect()
+        return fn(obj, client, *args, **kwargs)
+
+    return inner
+
+@resource_url(api, "/oauth/<string:service>", endpoint="oauth_api")
+class OAuth(Resource):
+    @with_service_client
+    def get(self, client):
+        url = client.session.authorization_url(client.auth_url, **client.auth_kwargs)[0]
         return { "auth_url": url }, 302, { "Location": url }
 
 
-@resource_url(api, "/authenticate/oauth/<string:service>/callback", endpoint="oauth_callback_api")
+@resource_url(api, "/oauth/<string:service>/callback", endpoint="oauth_callback_api")
 class OAuthCallback(Resource):
-    def get(self, service):
-        return { "success": True }
+    @with_service_client
+    def get(self, client):
+        parser = reqparse.RequestParser()
+        parser.add_argument("error", type="string", help="error message")
+        args = parser.parse_args()
+
+        if args["error"]:
+            return abort(400, message=error)
+
+        try:
+            email, verified = client.confirm_email(
+                client.session.fetch_token(
+                    client.token_url,
+                    client_secret=client.client_secret,
+                    authorization_response=flask.request.url))
+            if not verified:
+                abort(400, message="{} hasn't verified your email ({})".format(service, email))
+
+            return { "email": email }
+
+        except OAuth2Error as e:
+            return abort(400, message=str(e))
 
 
-class Google:
-    def __init__(self, scope=None):
-        self.client_id = config.get("GOOGLE_CLIENT_ID")
-        self.client_secret = config.get("GOOGLE_CLIENT_SECRET")
-        self.auth_url = config.get("GOOGLE_AUTH_URL")
-        self.token_url = config.get("GOOGLE_TOKEN_URL")
-        self.scope = scope or [ "https://www.googleapis.com/auth/userinfo.email",
-                                "https://www.googleapis.com/auth/userinfo.profile" ]
+class Client:
+    def __init__(self, service, client_id, client_secret, auth_url, token_url, auth_kwargs, session_kwargs):
+        self.service = service
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.auth_url = auth_url
+        self.token_url = token_url
+        self.auth_kwargs = auth_kwargs
 
         self.session = OAuth2Session(
             self.client_id,
-            scope=self.scope,
-            redirect_uri=api.url_for(OAuthCallback, api_version="-", service="google", _external=True))
+            redirect_uri=api.url_for(OAuthCallback, api_version="-", service=self.service, _external=True),
+            **session_kwargs
+        )
 
-    def auth_redirect(self):
-        return self.session.authorization_url(self.auth_url, prompt="select_account")[0]
+    def confirm_email(self, token=None):
+        raise NotImplementedError("Subclass must implement")
+
+
+class Google(Client):
+    def __init__(self):
+        super().__init__(
+            "google", config.get("GOOGLE_CLIENT_ID"), config.get("GOOGLE_CLIENT_SECRET"),
+            config.get("GOOGLE_AUTH_URL"), config.get("GOOGLE_TOKEN_URL"), { "prompt": "select_account" },
+            { "scope": [ "https://www.googleapis.com/auth/userinfo.email",
+                         "https://www.googleapis.com/auth/userinfo.profile" ]
+            }
+        )
+
+    def confirm_email(self, token=None):
+        id_ = self.session.get("https://www.googleapis.com/oauth2/v3/tokeninfo",
+                               params={"id_token": token.get("id_token")}).json()
+        return id_["email"], id_["email_verified"]
+
