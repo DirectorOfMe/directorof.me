e4e92018b5e0216ba807e7aaba173f11f5768d5c
Matthew Story <matt@directorof.me>
2018-04-13 05:27:55 +0000

    [DOM-09] restructure silly package into module
    Signed-off-by: Matthew Story <matt@directorof.me>

--- lib/python/flask_restful_ext/directorofme_flask_restful/fields.py
+++ lib/python/flask_restful_ext/directorofme_flask_restful/fields.py
@@ -1,13 +1,23 @@
 '''
-attributed_uri.py -- AttributedUrl
+fields.py -- ModelUrlList, AttributedUrl
 
 @author: Matthew Story <matt@directorof.me>
 
-XXX: Remove when flask_restful issue #702 is merged to a stable release
+XXX: Remove when flask_restful issue #714 is merged to a stable release
 '''
 from flask_restful import fields
 
-__all__ = [ "AttributedUrl" ]
+__all__ = [ "AttributedUrl", "ModelUrlList" ]
+
+class ModelUrlList(fields.List):
+    def __init__(self, *url_args, **url_kwargs):
+        return super().__init__(fields.Url(*url_args, **url_kwargs))
+
+    def format(self, model_list):
+        return super().format(
+            [fields.to_marshallable_type(val) for val in model_list]
+        )
+
 
 class AttributedUrl(fields.Url):
     def __init__(self, *args, **kwargs):
