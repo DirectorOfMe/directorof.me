02e9c9fc1b5a47272219308d3bc7c139a1227a1d
Matthew Story <matt@directorof.me>
2018-04-13 05:23:46 +0000

    [DOM-9] tests for utility methods
    Signed-off-by: Matthew Story <matt@directorof.me>

--- lib/python/flask_restful_ext/directorofme_flask_restful/utils.py
+++ lib/python/flask_restful_ext/directorofme_flask_restful/utils.py
@@ -6,12 +6,57 @@ utils.py -- Helper functions and misc bits
 
 import functools
 
-__all__ = [ "resource_url" ]
+import flask
+import flask_restful
+
+__all__ = [ "resource_url", "versioned_api" ]
 
 def resource_url(api, *args, **kwargs):
-	@functools.wraps(resource_url)
-	def real_decorator(cls):
-		api.add_resource(cls, *args, **kwargs)
-		return cls
+    '''Decorator for simplifying the add_resource interface:
 
-	return real_decorator
+       @resource_url(api, "/url/<foo>", endpoint="url")
+       class Thing(Resource):
+           def get(foo):
+                return { "foo": "bar" }
+    '''
+    @functools.wraps(resource_url)
+    def real_decorator(cls):
+        api.add_resource(cls, *args, **kwargs)
+        return cls
+
+    return real_decorator
+
+def store_version_in_a_global(api_version):
+    '''Default version storage method'''
+    flask.g.api_version = api_version
+
+def get_version_from_a_global():
+    '''Default version retrieval method'''
+    try:
+        return flask.g.api_version
+    except AttributeError:
+        return "-"
+
+def versioned_api(api_name, version_setter=store_version_in_a_global,
+                  version_getter=get_version_from_a_global):
+    '''Create a blueprint for a Flask-Restful API which respects a versioning
+       convention. Pre-processors can store this information somewhere, the
+       default is to store it to the `flask.g` variable for use by other
+       methods.'''
+    blueprint = flask.Blueprint(api_name, __name__, url_prefix="/api/<api_version>/{}".format(api_name))
+
+    @blueprint.url_value_preprocessor
+    def version_processor(endpoint, values):
+        '''Process the version variable so every end-point doesn't have to deal with it'''
+        version = values.pop("api_version")
+        if version_setter is not None:
+            version_setter(version)
+
+    @blueprint.url_defaults
+    def version_fetcher(endpoint, values):
+        '''Fetch the version when building a URL so we don't need to provide it manually'''
+        if "api_version" not in values:
+            values["api_version"] = version_getter()
+
+    return flask_restful.Api(blueprint)
+
