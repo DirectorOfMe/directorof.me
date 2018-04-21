cdfab36147ad2ff84e85c2f71a944a8718195281
Matthew Story <matt@directorof.me>
2018-04-21 21:29:08 +0000

    fix event tests
    Signed-off-by: Matthew Story <matt@directorof.me>

--- apis/event/tests/conftest.py
+++ apis/event/tests/conftest.py
@@ -1,10 +1,14 @@
 import pytest
 
 from directorofme_event import db as real_db
-from directorofme.authorization.orm import disable_permissions
+from directorofme.authorization.orm import disable_permissions as disable_permissions_decorator
 from directorofme.testing import db
 
 # setup the db fixture
-# TODO: autouse? 
+# TODO: autouse?
 db = pytest.fixture(autouse=True)(db(real_db))
-disable_permissions = pytest.fixture(disable_permissions)
+
+@pytest.fixture
+def disable_permissions():
+    with disable_permissions_decorator():
+        yield
