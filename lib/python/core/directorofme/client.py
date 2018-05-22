9a63579fae24e83a2b7c7d75ec4554f38bfa3044
Matthew Story <matt@directorof.me>
2018-05-22 06:50:48 +0000

    [DOM-88] with tests
    Signed-off-by: Matthew Story <matt@directorof.me>

---
+++ lib/python/core/directorofme/client.py
@@ -0,0 +1,79 @@
+from requests import Session
+from furl import furl
+
+__all__ = [ "Unauthorized", "PermissionDenied", "BadRequest", "NotFound", "ServerError", "DOM" ]
+
+class Unauthorized(Exception):
+    pass
+
+class PermissionDenied(Exception):
+    pass
+
+class BadRequest(Exception):
+    pass
+
+class NotFound(Exception):
+    pass
+
+class ServerError(Exception):
+    pass
+
+class DOM(Session):
+    def __init__(self, domain, version="-", access_token=None, access_csrf_token=None,
+                 refresh_token=None, refresh_csrf_token=None):
+
+        self.__url = furl("https://")
+        self.__url.host = domain
+        self.__url.path.segments = [ "api", version ]
+        super().__init__()
+
+        if access_csrf_token is not None:
+            self.headers["X-CSRF-TOKEN"] = access_csrf_token
+        if refresh_csrf_token is not None:
+            self.headers["X-CSRF-REFRESH-TOKEN"] = refresh_csrf_token
+
+        if access_token is not None:
+            self.cookies["access_token_cookie"] = access_token
+
+        if refresh_token is not None:
+            self.cookies["refresh_token_cookie"] = refresh_token
+
+    def url(self, url):
+        url_ = self.__url.copy()
+        url_.path.segments.extend(url.split("/"))
+        return url_.url
+
+    def check(self, response):
+        if response.status_code in (204, 304):
+            return None
+        elif response.status_code < 400:
+            return response.json()
+
+        errors = {
+            400: BadRequest,
+            401: Unauthorized,
+            403: PermissionDenied,
+            404: NotFound
+        }
+
+        if response.status_code in errors:
+            raise errors[response.status_code](response.json().get("message"))
+        raise ServerError(response.text)
+
+    def get(self, url, *args, **kwargs):
+        return self.check(super().get(url=self.url(url), *args, **kwargs))
+
+    def post(self, url, *args, **kwargs):
+        return self.check(super().post(url=self.url(url), *args, **kwargs))
+
+    def put(self, url, *args, **kwargs):
+        return self.check(super().put(url=self.url(url), *args, **kwargs))
+
+    def delete(self, url, *args, **kwargs):
+        return self.check(super().delete(url=self.url(url), *args, **kwargs))
+
+    def patch(self, url, *args, **kwargs):
+        return self.check(super().patch(url=self.url(url), *args, **kwargs))
+
+    def refresh(self):
+        return self.get("auth/refresh")
---
+++ lib/python/core/tests/test__directorofme_client.py
@@ -0,0 +1,84 @@
+import pytest
+import json
+import requests
+
+from unittest import mock
+from directorofme import client
+
+DOM = client.DOM
+
+@pytest.fixture
+def dom_client():
+    yield DOM("test.directorof.me")
+
+class TestDOM:
+    def test__init__(self):
+        assert "X-CSRF-TOKEN" not in DOM("test.directorof.me").headers, "no headers set if no CSRF passed"
+        assert "X-CSRF-REFRESH-TOKEN" not in DOM("test.directorof.me").headers, "no headers set if no CSRF passed"
+
+        assert DOM("test.directorof.me").cookies == {}, "no headers set if no tokens passed"
+
+        assert DOM("test.directorof.me", access_csrf_token="csrf").headers["X-CSRF-TOKEN"] == "csrf", \
+               "headers set when csrf passed"
+        assert DOM("test.directorof.me", refresh_csrf_token="csrf").headers["X-CSRF-REFRESH-TOKEN"] == "csrf", \
+               "headers set when csrf passed"
+
+        cookies = DOM("test.directorof.me", access_token="access", refresh_token="refresh").cookies
+        assert cookies["access_token_cookie"] == "access", "cookies set from pased tokens"
+        assert cookies["refresh_token_cookie"] == "refresh", "cookies set from pased tokens"
+
+    def test__url(self):
+        assert DOM("test.directorof.me").url("api_name/api_endpoint") == \
+               "https://test.directorof.me/api/-/api_name/api_endpoint", "url works with default version"
+        assert DOM("test.directorof.me", version="3").url("api_name/api_endpoint") == \
+               "https://test.directorof.me/api/3/api_name/api_endpoint", "url works with passed version"
+
+    def test__check(self, dom_client):
+        response = requests.Response()
+        response._content = bytes(json.dumps({ "message": "message" }), "utf8")
+
+        for status_code in (204, 304):
+            response.status_code = status_code
+            assert dom_client.check(response) is None, "*04s return None"
+
+        for status_code in (200, 201, 301, 302):
+            response.status_code = status_code
+            assert dom_client.check(response) == { "message": "message" }, "success status codes return objects"
+
+        response.status_code = 400
+        with pytest.raises(client.BadRequest, match=r".*message.*"):
+            dom_client.check(response)
+
+        response.status_code = 401
+        with pytest.raises(client.Unauthorized, match=r".*message.*"):
+            dom_client.check(response)
+
+        response.status_code = 403
+        with pytest.raises(client.PermissionDenied, match=r".*message.*"):
+            dom_client.check(response)
+
+        response.status_code = 404
+        with pytest.raises(client.NotFound, match=r".*message.*"):
+            dom_client.check(response)
+
+        response.status_code = 500
+        response._content = b"text"
+        with pytest.raises(client.ServerError, match=r".*text.*"):
+            dom_client.check(response)
+
+    def test__verbs(self, dom_client):
+        for method in ("get", "post", "put", "patch", "delete"):
+            response = requests.Response()
+            response.status_code = 200
+            response._content = bytes(json.dumps({ "test":  method }), "utf8")
+
+            with mock.patch("requests.Session.{}".format(method)) as mock_method:
+                mock_method.return_value = response
+                assert getattr(dom_client, method)("foo/bar") == { "test": method }, "method pass-through works"
+                mock_method.assert_called_with(url="https://test.directorof.me/api/-/foo/bar")
+
+    def test__refresh(self, dom_client):
+        with mock.patch.object(dom_client, "get") as mock_get:
+            mock_get.return_value = { "session": True }
+            assert dom_client.refresh() == { "session": True }, "refresh calls get"
+            mock_get.assert_called_with("auth/refresh")
